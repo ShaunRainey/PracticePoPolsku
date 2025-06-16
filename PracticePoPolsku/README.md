b04a242e00ab9fdeae1dfd43aaa1dcde524a4665
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-06-16 11:29:47 +0100

    Start project + complete sentence utilities

---
+++ PracticePoPolsku/README.md
@@ -0,0 +1,3 @@
+1) Created Utilities folder to start organising API calls
+2) npm install axios
+    - This will allow simplified API requests
---
+++ PracticePoPolsku/src/Utilities/sentences.js
@@ -0,0 +1,30 @@
+
+import axios from 'axios';
+
+const polishAPI = axios.create({ baseURL: "https://practice-po-polsku.onrender.com/api/sentences" })
+
+function getSentences(query) {
+
+    let searchString = "";
+    
+    if(query){
+        searchString += `?unit=${query}`
+    }
+
+    return polishAPI.get(searchString).then((response) => {
+        return response.data
+    })
+}
+
+function getSentenceById(id) {
+    return polishAPI.get(`/${id}`).then((response) => {
+        return response.data
+    })    
+}
+
+export default {
+    getSentences,
+    getSentenceById
+}
+
+getSentences(2.25).then(response => console.log(response))
